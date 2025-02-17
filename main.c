a2331188d721f8ee24edf8f21a517272cefb839c
ArinPrajapati <arinprajapati78@gmail.com>
2025-02-17 19:27:09 +0530

    editor promt for newfiles

--- main.c
+++ main.c
@@ -32,6 +32,8 @@
 
 /** function prototypes **/
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 enum editorKey
 {
@@ -56,9 +58,8 @@ typedef struct erow
     char *chars;
     char *render;
 
+    // global struct to store the editor configuration.
 } erow;
-
-// global struct to store the editor configuration.
 struct editorConfig
 {
     int cx, cy;
@@ -350,14 +351,13 @@ void editorUpdateRow(erow *row)
     row->rsize = idx;
 }
 
-void editorAppendRows(char *s, size_t len)
+void editorInsertRow(int at, char *s, size_t len)
 {
-    erow *new_row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
-    if (new_row == NULL)
-        die("realloc");
-    E.row = new_row;
 
-    int at = E.numrows;
+    if (at < 0 || at > E.numrows)
+        return;
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
 
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
@@ -448,10 +448,31 @@ void editorInsertChars(int c)
         // the E.cy is the y coordinate of the cursor.
         // the E.numrows is the number of rows.
         // if both are equal, we append a new row.
-        editorAppendRows("", 0);
+        editorInsertRow(E.numrows, "", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
+}
+
+void editorInsertNewline()
+{
+    if (E.cx == 0)
+    {
+        editorInsertRow(E.cy, "", 0);
+    }
+    else
+    {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        // the '\0' character is used to terminate the string.
+        editorUpdateRow(row);
+    }
+
+    E.cy++;
+    E.cx = 0;
 }
 
 void editorDelChar()
@@ -523,7 +544,7 @@ void editorOpen(char *filename)
         {
             linelen--;
         }
-        editorAppendRows(line, linelen);
+        editorInsertRow(E.numrows, line, linelen);
     }
     free(line);
     fclose(fp);
@@ -532,8 +553,16 @@ void editorOpen(char *filename)
 
 void editorSave()
 {
+
     if (E.filename == NULL)
-        return;
+    {
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        if (E.filename == NULL)
+        {
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
+    }
 
     int len;
     char *buf = editorRowsString(&len);
@@ -609,6 +638,54 @@ void abFree(struct abuf *ab)
 }
 
 /** input ***/
+
+char *editorPrompt(char *prompt)
+{
+    size_t bufsize = 128;
+    // size_t is an unsigned integer type that is used to represent the size of objects in bytes.
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while (1)
+    {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == Del_Key || c == CTRL_KEY('h') || c == Back_Space)
+        {
+            if (buflen != 0)
+                buf[--buflen] = '\0';
+        }
+        else if (c == '\x1b')
+        {
+            editorSetStatusMessage("");
+            free(buf);
+            return NULL;
+        }
+        else if (c == '\r')
+        {
+            // which is enter.
+            if (buflen != 0)
+            {
+                editorSetStatusMessage("");
+                return buf;
+            }
+        }
+        else if (!iscntrl(c) && c < 128)
+        {
+            if (buflen == bufsize - 1)
+            {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+}
 
 void editorSetStatusMessage(const char *fmt, ...)
 {
@@ -688,7 +765,8 @@ void editorProcessKeypress()
     {
 
     case '\r':
-        // the '\r' character is the carriage return character.
+        // the '\r' character is the carriage return character that is "Enter".
+        editorInsertNewline();
         break;
 
     case CTRL_KEY('q'):
